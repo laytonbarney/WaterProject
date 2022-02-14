146c4bc671241c89c6d29b95e6fc0b356f9c8566
Layton Barney <laytonb2@byu.edu>
2022-02-14 16:48:53 -0700

    Add project files.

---
+++ WaterProject/Models/EFWaterProjectRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterProject.Models
+{
+    public class EFWaterProjectRepo : IWaterProjectRepo
+    {
+
+        private WaterProjectContext context { get; set; }
+
+        public EFWaterProjectRepo (WaterProjectContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Project> Projects => context.Projects;
+    }
+}
---
+++ WaterProject/Models/IWaterProjectRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterProject.Models
+{
+    public interface IWaterProjectRepo
+    {
+        IQueryable<Project> Projects { get; }
+
+    }
+}
